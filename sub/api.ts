e2c9068a67e3c67f78c34072a7c524cfeed2221f
shintarou37 <m.sintarou37@gmail.com>
2021-10-30 13:32:21 +0900

    結果加工

--- sub/api.ts
+++ sub/api.ts
@@ -1,10 +1,12 @@
 const { execSync } = require('child_process');
+var fs = require('fs');
 const constant= require('./.token');
 const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1'
 const key = 'key'
 const ramen_code = "G013"
 const tokyo_code = "SA11"
-let a = execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=2&service_area=${tokyo_code}&format=json"`, {maxBuffer: 10000 * 10000});
+var results_api = JSON.stringify(JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=2&service_area=${tokyo_code}&order=1&format=json"`, {maxBuffer: 10000 * 10000})));
+// console.log(results_api)
 
-var aa = JSON.parse(a)
-console.log(JSON.stringify(aa))
+// for(let i : number = 0; i < num; i++);
+fs.writeFileSync('sub/result.json', results_api);
