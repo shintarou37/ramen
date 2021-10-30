d68844b3991d060544a37c074135b183d4046c15
shintarou37 <m.sintarou37@gmail.com>
2021-10-30 20:01:43 +0900

    ラーメンテーブル作成

---
+++ sub/db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `ramen`.`api` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `open` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
