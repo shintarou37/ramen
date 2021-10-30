07397282a49e35465d73c3627035ed82d6ebba75
shintarou37 <m.sintarou37@gmail.com>
2021-10-30 12:32:17 +0900

    東京限定

--- sub/api.ts
+++ sub/api.ts
@@ -3,7 +3,8 @@ const constant= require('./.token');
 const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1'
 const key = 'key'
 const ramen_code = "G013"
-let a = execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=2&format=json"`, {maxBuffer: 10000 * 10000});
+const tokyo_code = "SA11"
+let a = execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=2&service_area=${tokyo_code}&format=json"`, {maxBuffer: 10000 * 10000});
 
 var aa = JSON.parse(a)
 console.log(JSON.stringify(aa))
