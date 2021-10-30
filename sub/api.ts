1ba34cf33e4c64faec7a7d33628f242b9ebb2244
shintarou37 <m.sintarou37@gmail.com>
2021-10-30 20:41:35 +0900

    最大取得値設定

--- sub/api.ts
+++ sub/api.ts
@@ -1,11 +1,13 @@
 const { execSync } = require('child_process');
 var fs = require('fs');
-const constant= require('./.token');
+var constant = require('./.token');
 const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1'
-const key = 'key'
 const ramen_code = "G013"
 const tokyo_code = "SA11"
-var results_api = JSON.stringify(JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=2&service_area=${tokyo_code}&order=1&format=json"`, {maxBuffer: 10000 * 10000})));
+const count : number = 30
+
+
+var results_api = JSON.stringify(JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=${count}&service_area=${tokyo_code}&order=1&format=json"`, {maxBuffer: 10000 * 10000})));
 // console.log(results_api)
 
 // for(let i : number = 0; i < num; i++);
