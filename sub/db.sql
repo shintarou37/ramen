be6d07567455dbf299636df26591546887f18285
shintarou37 <m.sintarou37@gmail.com>
2021-11-01 22:29:46 +0900

    中間エリア追加

--- sub/db.sql
+++ sub/db.sql
@@ -2,6 +2,7 @@ CREATE TABLE `ramen`.`api` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `address` VARCHAR(100) NOT NULL,
+  `middle_area` VARCHAR(50) NOT NULL,
   `open` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
