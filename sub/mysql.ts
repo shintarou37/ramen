3f118adbce3f14fbcad84466144687a405ce0bf5
shintarou37 <m.sintarou37@gmail.com>
2021-11-04 01:03:17 +0900

    api Table変更

--- sub/mysql.ts
+++ sub/mysql.ts
@@ -10,18 +10,24 @@ connection = mysql.createConnection({
   database : constant.schema
 });
 // レコード全削除
-// var sql = `truncate table ramen.api`
-// var sql = `truncate table ramen.middle_area`
+// var sql = `truncate table ramen.api;`
+// var sql = `truncate table ramen.middle_area;`
 
 var sql : string = ""
+// ミッドエリアSQL
 for(let i : number = 0; i < result.length; i++){
   if(! sql.includes(`${result[i].middle_area.name}`))
     sql += `INSERT INTO ${constant.schema}.middle_area(name) values("${result[i].middle_area.name}");`;
 }
+// apiテーブルSQL
+for(let i : number = 0; i < result.length; i++){
+  sql += `INSERT INTO ${constant.schema}.api(id,name,address,open,url,middle_area_id) values("${i + 1}","${result[i].name}","${result[i].address}","${result[i].open}","${result[i].coupon_urls.pc}","${result[i].middle_area.name}");`;
+}
+console.log(sql)
 // connection.query(sql, function (error:any, results:any, fields:any) {
 //   console.log(error)
 //   console.log(results)
 //   console.log(fields)
 // });
 
-// connection.end()
+connection.end()
