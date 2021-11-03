d2d84c6e99041f463aef9594ee0d89cffaf69131
shintarou37 <m.sintarou37@gmail.com>
2021-11-04 01:04:15 +0900

    結果の加工

--- sub/api.ts
+++ sub/api.ts
@@ -11,14 +11,16 @@ const options = {
 };
 
 var re : any 
-for(let i : number = 1; i < 150; i += 30){
-  console.log(i)
-  var results_api : any = JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=${count}&service_area=${tokyo_code}&order=1&start=${i}&format=json"`, {maxBuffer: 10000 * 10000}));
-  re += (JSON.stringify(results_api.results.shop))
-  re += ","
-}
-fs.writeFileSync('sub/result.json', re, options, (err:any) => {
-  if (err) throw err;
-  console.log('正常に書き込みが完了しました');
-});
-// var result_number  = results_api.results.shop.length
+// for(let i : number = 1; i < 15000; i += 30){
+//   console.log(`${i} iの回数`)
+//   var results_api : any = JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&count=${count}&service_area=${tokyo_code}&order=1&start=${i}&format=json"`, {maxBuffer: 10000 * 10000}));
+//   re += (JSON.stringify(results_api.results.shop))
+//   re += ","
+//   console.log(`${results_api.results.shop.length}`)
+// }
+// fs.writeFileSync('sub/result.json', re, options, (err:any) => {
+//   if (err) throw err;
+//   console.log('正常に書き込みが完了しました');
+// });
+var results_api : any = JSON.parse(execSync(`curl "${url}?key=${constant.token}&genre=${ramen_code}&address=東京都&count=${count}&order=1&start=71&format=json"`, {maxBuffer: 10000 * 10000}));
+console.log(JSON.stringify(results_api))
