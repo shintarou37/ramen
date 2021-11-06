c9f6a94e683d4ab274dcdb4e70e6f86da8721e25
shintarou37 <m.sintarou37@gmail.com>
2021-11-06 23:01:49 +0900

    expected a function error 修正

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,23 @@
 var express = require('express');
+var async = require('async');
+const db = require('../models/index');
 var router = express.Router();
+// models
+router.get('/', (req:any, res:any, next:any) => {
+console.log(db.api)
+  // async.series([
+  //   db.api
+  // ], function (err:any, results:any) {
+  //     if (err) {
+  //         return next(err);
+  //     }
+  //     res.render('index', { title: 'Express' });
+  // });    
+});
 
-/* GET home page. */
-router.get('/', function(req:any, res:any, next:any) {
+router.get('/test', (req:any, res:any, next:any) =>  {
+  console.log(db.middle_area)
+  db.middle_area.test()
   res.render('index', { title: 'Express' });
 });
 
