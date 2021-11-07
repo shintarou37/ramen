9559806ed02d23e0d32d76fa73a5b3a5f652294b
shintarou37 <m.sintarou37@gmail.com>
2021-11-07 22:28:00 +0900

    model typescript sql成功

--- models/user.ts
+++ models/user.ts
@@ -1,28 +1,107 @@
-'use strict';
-var {
-  Model
-} = require('sequelize');
-module.exports = (sequelize:any, DataTypes:any) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models:any) {
-      // define association here
+import {
+  Sequelize,
+  Model,
+  DataTypes,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  Association,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+} from "sequelize";
+
+const sequelize = new Sequelize("mysql://root@localhost:3306/ramen");
+
+// These are all the attributes in the User model
+// interface UserAttributes {
+//   id: number;
+//   name: string;
+//   pass: string;
+//   status: number;
+//   createdAt:Date;
+//   updatedAt:Date;
+// }
+
+// Some attributes are optional in `User.build` and `User.create` calls
+
+class User extends Model
+ {
+  public id!: number; // Note that the `null assertion` `!` is required in strict mode.
+  public name!: string;
+  public pass!: string;
+  public status!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    pass: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    status: {
+      type: new DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: new DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: new DataTypes.DATE,
+      allowNull: false,
     }
-  };
-  user.init({
-    id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    status: DataTypes.INTEGER,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'user',
+  },
+  {
+    tableName: "users",
+    sequelize, // passing the `sequelize` instance is required
+  }
+);
+new User
+async function doStuff() {
+  const instance = await User.findByPk(1, {
+    rejectOnEmpty: true,
   });
-  return user;
-};
+  console.log(instance);
+}
+doStuff()
+
+// 'use strict';
+// var {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize:any, DataTypes:any) => {
+//   class user extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models:any) {
+//       // define association here
+//     }
+//   };
+//   user.init({
+//     id: DataTypes.INTEGER,
+//     name: DataTypes.STRING,
+//     pass: DataTypes.STRING,
+//     status: DataTypes.INTEGER,
+//     createdAt: DataTypes.DATE,
+//     updatedAt: DataTypes.DATE
+//   }, {
+//     sequelize,
+//     modelName: 'user'
+//   });
+//   return user;
+// };
