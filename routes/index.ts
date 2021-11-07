1050d1676a910e4a8fc336280b97a200981d2926
shintarou37 <m.sintarou37@gmail.com>
2021-11-08 00:49:06 +0900

    DBから取得した値をレンダリング

--- routes/index.ts
+++ routes/index.ts
@@ -1,31 +1,16 @@
 var express = require('express');
-var async = require('async');
-var aa = require('../models/index');
-var models = require('../models');
-var middle_area_test = require('../models/middle_area');
-var tee = require('../models/test');
 var router = express.Router();
-// aa.middle_area
-middle_area_test.te()
+import {
+  User,
+  getUser,
+} from "../models/user";
 
+router.get('/', (req:any, res:any, next:any) =>  {
+  (async () => {
+    var result = await getUser()
+    console.log(result.dataValues)
+    res.render('index', { title: 'Express' ,name: result.dataValues.name});
+  })();
+});
 
-
-// router.get('/', (req:any, res:any, next:any) => {
-// // console.log(aa.api)
-//   // async.series([
-//   //   db.api
-//   // ], function (err:any, results:any) {
-//   //     if (err) {
-//   //         return next(err);
-//   //     }
-//   //     res.render('index', { title: 'Express' });
-//   // });    
-// });
-
-// router.get('/test', (req:any, res:any, next:any) =>  {
-//   // tee.say()
-//   // middle_area_test.say()
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
+module.exports = router;
