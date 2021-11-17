edb67e1d6b33dd9cd6f3990bde8e866abc55bff4
shintarou37 <m.sintarou37@gmail.com>
2021-11-18 00:39:44 +0900

    index routes 修正

--- routes/index.ts
+++ routes/index.ts
@@ -5,30 +5,10 @@ var models = require('../models');
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
     var result = await models.default.MiddleArea.index()
-    console.log(JSON.stringify(result[0].Apis[0]))
+    console.log(JSON.stringify(result))
     await res.render('index', { result: result});
   })();
 });
-// router.get('/mi', (req:any, res:any, next:any) =>  {
-//   (async () => {
-//     var result = await getIndex()
-//     console.log(result)
-//     res.render('index', { title: 'Express',name:"aa"});
-//   })();
-// });
-router.get('/api', (req:any, res:any, next:any) =>  {
-  (async () => {
-    var result = await models.default.Api.te()
-    res.render('index', { title: 'Express',name:"aa"});
-  })();
-});
 
-// router.get('/user', (req:any, res:any, next:any) =>  {
-//   (async () => {
-//     var result = await getUser()
-//     console.log(result.dataValues)
-//     res.render('index', { title: 'Express' ,name: result.dataValues.name});
-//   })();
-// });
 
 module.exports = router;
