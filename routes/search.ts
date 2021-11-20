18436ac099fda25dad270f6a6b6a495e70ba6263
shintarou37 <m.sintarou37@gmail.com>
2021-11-21 00:56:01 +0900

    add serch result

--- routes/search.ts
+++ routes/search.ts
@@ -4,10 +4,12 @@ var models = require('../models');
 
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
-    req.query.name ? "": req.query.name = "%"
-    var result = await models.default.Api.search(req.query.name,req.query.area)
-    // console.log(JSON.stringify(result))
-    await res.render('search', { result: result});
+    var name : string
+    req.query.name ? name = req.query.name: name = "%"
+    var drop = await models.default.MiddleArea.index()
+    var results = await models.default.Api.search(name,req.query.area)
+    console.log(JSON.stringify(results))
+    await res.render('search', { results: results, drop: drop, search_name:req.query.name, search_area:req.query.area});
   })();
 });
 module.exports = router;
