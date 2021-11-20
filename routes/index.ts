ca0d64436791034ee5b2023c72bbb393cc6e39bb
shintarou37 <m.sintarou37@gmail.com>
2021-11-21 00:55:29 +0900

    change result variable

--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
-    var result = await models.default.MiddleArea.index()
-    console.log(JSON.stringify(result))
-    await res.render('index', { result: result});
+    var drop = await models.default.MiddleArea.index()
+    // console.log(JSON.stringify(result))
+    await res.render('index', { drop: drop});
   })();
 });
 
