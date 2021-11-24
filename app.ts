d57c6c1f5d752dfc65ab6d265bf9f8c5551acc60
shintarou37 <m.sintarou37@gmail.com>
2021-11-25 00:30:21 +0900

    app編集

--- app.ts
+++ app.ts
@@ -6,9 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var searchRouter = require('./routes/search');
+var likeRouter = require('./routes/like');
 var userRouter = require('./routes/user');
 
 var app = express();
+
+app.use(express.static('images'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/search', searchRouter);
+app.use('/like', likeRouter);
 app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
