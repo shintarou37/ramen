bd415d69582238638f95d30e66e8eca7e6ffb992
shintarou37 <m.sintarou37@gmail.com>
2021-11-27 22:48:54 +0900

    add basic pagenation func

--- routes/search.ts
+++ routes/search.ts
@@ -4,12 +4,19 @@ var models = require('../models');
 
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
+    var current_page = 1;
+    if (typeof req.query.page !== 'undefined' && req.query.page && isFinite(req.query.page) && Number(req.query.page) > 0) {
+        current_page =+ req.query.page;
+    }
+    var offset = (current_page - 1) * 20;
     var name : string
     req.query.name ? name = `%${req.query.name}%`: name = "%"
     var drop = await models.default.MiddleArea.index()
-    var results = await models.default.Api.search(name,req.query.area)
-    console.log(JSON.stringify(results[0]))
-    await res.render('search', { results: results, drop: drop, search_name:req.query.name, search_area:req.query.area});
+    var results = await models.default.Api.search(name, req.query.area, offset)
+    console.log(JSON.stringify(results.rows))
+    await res.render('search', { 
+      results: results.rows, count:results.count, drop: drop, search_name:req.query.name, search_area:req.query.area, current_page: current_page
+    });
   })();
 });
 module.exports = router;
