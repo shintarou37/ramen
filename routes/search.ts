f86c504317a5794ac33c3988c0d1a67e879ed0a1
shintarou37 <m.sintarou37@gmail.com>
2021-11-27 14:47:22 +0900

    change search css

--- routes/search.ts
+++ routes/search.ts
@@ -5,10 +5,10 @@ var models = require('../models');
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
     var name : string
-    req.query.name ? name = req.query.name: name = "%"
+    req.query.name ? name = `%${req.query.name}%`: name = "%"
     var drop = await models.default.MiddleArea.index()
     var results = await models.default.Api.search(name,req.query.area)
-    console.log(JSON.stringify(results))
+    console.log(JSON.stringify(results[0]))
     await res.render('search', { results: results, drop: drop, search_name:req.query.name, search_area:req.query.area});
   })();
 });
