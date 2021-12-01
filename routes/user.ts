c739dc54c09ef38fe987ef9750645b2e942342f7
shintarou37 <m.sintarou37@gmail.com>
2021-12-02 01:00:41 +0900

    add basic sign_up func

--- routes/user.ts
+++ routes/user.ts
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req:any, res:any, next:any) {
-  res.send('respond with a resource');
-});
+router.get('/sign_in', (req:any, res:any, next:any) =>  {
+  (async () => {
 
+  })();
+});
+router.get('/sign_up', (req:any, res:any, next:any) =>  {
+  (async () => {
+    console.log("req.params.id")
+
+    await res.render('sign_up');
+  })();
+});
 module.exports = router;
