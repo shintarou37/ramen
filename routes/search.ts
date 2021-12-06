962985bcc8b5eff10dfab0a8d26c3759377f4507
shintarou37 <m.sintarou37@gmail.com>
2021-12-06 23:22:44 +0900

    change variable definition

--- routes/search.ts
+++ routes/search.ts
@@ -4,17 +4,18 @@ var models = require('../models');
 
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
-    var current_page = 1;
+    let current_page = 1;
     if (typeof req.query.page !== 'undefined' && req.query.page && isFinite(req.query.page) && Number(req.query.page) > 0) {
         current_page =+ req.query.page;
     }
-    var offset = (current_page - 1) * 20;
-    var name : string
+    let offset = (current_page - 1) * 20;
+    let name : string
     req.query.name ? name = `%${req.query.name}%`: name = "%"
-    var drop = await models.default.MiddleArea.index()
-    var results = await models.default.Api.search(name, req.query.area, offset)
+    let drop = await models.default.MiddleArea.index()
+    let results = await models.default.Api.search(name, req.query.area, offset)
     await res.render('search', { 
-      results: results.rows, count:results.count, drop: drop, search_name:req.query.name, search_area:req.query.area, current_page: current_page
+      results: results.rows, count:results.count, drop: drop, search_name:req.query.name, search_area:req.query.area, 
+      current_page: current_page, req: req
     });
   })();
 });
