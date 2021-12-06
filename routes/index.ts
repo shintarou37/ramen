9dba192e1b9385cbbfd4fcb2346d3e6e7b01612a
shintarou37 <m.sintarou37@gmail.com>
2021-12-06 22:56:11 +0900

    add mypage a tage

--- routes/index.ts
+++ routes/index.ts
@@ -3,9 +3,11 @@ var router = express.Router();
 var models = require('../models');
 router.get('/', (req:any, res:any, next:any) =>  {
   (async () => {
+  console.log(req.session)
+
     var drop = await models.default.MiddleArea.index()
     // console.log(JSON.stringify(result))
-    await res.render('index', { drop: drop, search_name: "", search_area: ""});
+    await res.render('index', { drop: drop, search_name: "", search_area: "", req: req});
   })();
 });
 router.get('/test', (req:any, res:any, next:any) =>  {
