0428645abfa2bc43aa46099c9a59fcd00a4fdbe3
shintarou37 <m.sintarou37@gmail.com>
2022-02-24 23:37:48 +0900

    fix/argument

--- utilities/session.ts
+++ utilities/session.ts
@@ -2,6 +2,6 @@ exports.login_confirmation = (req:any, res:any, next:any) =>{
   if(req.session.user){
     next();
   }else{
-    res.redirect('/user/sign_in');
+    res.redirect('/user/sign_in',{req: req});
   }
 }
