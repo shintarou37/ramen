385327c991260c0f2cea93d22e5783a8eb636790
shintarou37 <m.sintarou37@gmail.com>
2022-02-27 12:39:16 +0900

    change/types

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const router = express.Router();
 var models = require('../models');
-router.get('/', (req:any, res:any, next:any) =>  {
+router.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
   (async () => {
     // console.log(req.session.user)
     var drop = await models.default.MiddleArea.index()
-    await res.render('index', {drop: drop, search_name: "", search_area: "", req: req});
+    res.render('index', {drop: drop, search_name: "", search_area: "", req: req});
   })();
 });
 
