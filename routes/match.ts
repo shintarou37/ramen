674edd7ad004f23bb08c1a8457f7638946d9a48a
shintarou37 <m.sintarou37@gmail.com>
2022-03-12 14:47:03 +0900

    Update/change_user_name layout

--- routes/match.ts
+++ routes/match.ts
@@ -7,7 +7,6 @@ const session = require("../utilities/session");
 // マッチする
 router.get('/', session.login_confirmation, (req: express.Request, res: express.Response, next: express.NextFunction) => {
     (async () => {
-        console.log("--------------matchに入りました")
         const result = await models.default.Match.register(req);
         await models.default.Like.delete(req);
         res.redirect(`/message/${result.id}`);
@@ -17,7 +16,6 @@ router.get('/', session.login_confirmation, (req: express.Request, res: express.
 // マイページ
 router.get('/index/:id', session.login_confirmation, (req: express.Request, res: express.Response, next: express.NextFunction) => {
     (async () => {
-        console.log("----------------------マイページに入りました")
         let user_id: number = Number(req.params.id);
         let results = await models.default.Match.index(user_id);
         res.render('user/my_page', {req: req, results: results});
